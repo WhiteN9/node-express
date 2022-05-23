9aae02742b88a8f89f4937188a9221e9e7353cc8
White Nguyen <whitenguyen0909@gmail.com>
2022-05-23 10:43:35 -0600

    added app-level function and use morgan to log

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Home Page");
+  res.send("Home Page");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Users Page");
+});
+
+function sayHello(req, res, next) {
+  res.send("Hello!");
+}
+
+// const logging = (req, res, next) => {
+//   console.log("A request is being made !");
+//   next();
+// };
+// app.use(logging);
+
+app.use(morgan("dev"));
+app.use(sayHello);
 
 module.exports = app;
