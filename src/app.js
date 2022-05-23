99126301db51ed306fe181f423fb7e7ee8d3c208
White Nguyen <whitenguyen0909@gmail.com>
2022-05-23 14:12:41 -0600

    added query and route parameters, organized the files in order

--- src/app.js
+++ src/app.js
@@ -3,30 +3,58 @@ const morgan = require("morgan");
 
 const app = express();
 
+// Application-level middleware
+app.use(morgan("dev"));
+
+// Route functions
 function sayHello(req, res, next) {
   res.send("Hello!");
 }
 
-app.use(morgan("dev"));
+const sayHi = (req, res) => {
+  // console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : " Hello!";
+  res.send(content);
+};
 
+const saySomething = (req, res) => {
+  // console.log(req.params);
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+  const content = greeting && `${greeting}, ${name}!`;
+  res.send(content);
+};
+
+// Route functions
 app.get("/", (req, res) => {
   console.log("Home Page");
   res.send("Home Page");
 });
 
 app.get("/users", (req, res) => {
-  console.log("Users Page")
+  console.log("Users Page");
   res.send("Users Page");
 });
 
-// const logging = (req, res, next) => {
-//   console.log("A request is being made !");
-//   next();
-// };
-// app.use(logging);
+app.get("/songs", (req, res) => {
+  const title = req.query.title;
+  res.send(title);
+});
+  // const logging = (req, res, next) => {
+  //   console.log("A request is being made !");
+  //   next();
+  // };
+  // app.use(logging);
 
-// app.use(sayHello);
+  // app.use(sayHello);
 
 app.get("/hello", sayHello);
+app.get("/hi", sayHi);
+app.get("/say/goodbye", (req, res) => {
+  res.send("Sorry to see you go!");
+});
+app.get("/say/:greeting", saySomething);
+
 
 module.exports = app;
