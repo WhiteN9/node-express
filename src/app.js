6233488e3b96dcbfcf3e5d09a7c1ac07645c0647
White Nguyen <whitenguyen0909@gmail.com>
2022-05-23 21:19:27 -0600

    added router-level middle ware

--- src/app.js
+++ src/app.js
@@ -7,56 +7,57 @@ const app = express();
 app.use(morgan("dev"));
 
 // Route functions
-function sayHello(req, res, next) {
-  res.send("Hello!");
-}
-
-const sayHi = (req, res) => {
-  // console.log(req.query);
-  const name = req.query.name;
-  const content = name ? `Hello, ${name}!` : " Hello!";
-  res.send(content);
-};
-
 const saySomething = (req, res) => {
-  // console.log(req.params);
   const greeting = req.params.greeting;
   const name = req.query.name;
   const content = greeting && `${greeting}, ${name}!`;
   res.send(content);
 };
 
-// Route functions
-app.get("/", (req, res) => {
-  console.log("Home Page");
-  res.send("Home Page");
-});
+// Router-level middleware
+const checkForAbbreviationLength = (req, res, next) => {
+  const abbreviation = req.params.abbreviation;
+  if (abbreviation.length > 2) {
+    next(`State abbreviation ${abbreviation} is invalid.`);
+  } else {
+    next();
+  }
+};
 
-app.get("/users", (req, res) => {
-  console.log("Users Page");
-  res.send("Users Page");
-});
-
+// Routes
 app.get("/songs", (req, res) => {
   const title = req.query.title;
   res.send(title);
 });
-
-app.get("/hello", sayHello);
-app.get("/hi", sayHi);
 app.get("/say/goodbye", (req, res) => {
   res.send("Sorry to see you go!");
 });
 app.get("/say/:greeting", saySomething);
 
-app.get("/states/:abbreviation", (req, res, next) => {
-  const abbreviation = req.params.abbreviation;
-  if (abbreviation.length > 2) {
-    next("State abbreviation is invalid.");
-  } else {
-    res.send(`${abbreviation} is a nice state, I'd like to visit.`);
+
+app.get(
+  "/states/:abbreviation",
+  checkForAbbreviationLength,
+  (req, res, next) => {
+    res.send(`${req.params.abbreviation} is a nice state, I'd like to visit.`);
   }
-});
+);
+
+app.get(
+  "/travel/:abbreviation",
+  checkForAbbreviationLength,
+  (req, res, next) => {
+    res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
+  }
+);
+
+  // Why not just include a line like the following in the server code?
+  // app.use(checkForAbbreviationLength);
+  // Because it will match with any routes, such as:
+  // http://localhost:5000/random-routes
+  // We get TypeError: Cannot read property 'length' of undefined.
+  // Because abbreviation requires an URL with a params named `abbreviation`.
+
 
 // Not-found handler
 app.use((req, res, next) => {
